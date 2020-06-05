ca219c90ee18ab07db9f9417d3e3a1afd78dff49
ambitionye <16633887600@qq.com>
2020-06-05 15:31:06 +0800

    只要分类和年龄范围

--- findel_crawl/findel_crawl/items.py
+++ findel_crawl/findel_crawl/items.py
@@ -12,11 +12,5 @@ class findelItem(scrapy.Item):
     
     link = scrapy.Field()     #链接
     classsify = scrapy.Field()#目录路径，需要每个类别用/分开
-    img = scrapy.Field()      #缩略图
-    #img_clear = scrapy.Field()#清晰图调整with即可
     title = scrapy.Field() #商品标题
     code = scrapy.Field()   #商品编号
-    price = scrapy.Field()    #商品价格
-    describe = scrapy.Field() #商品描述 (需要进入页面)
-    nature = scrapy.Field()   #商品属性（颜色）(需要进入页面)
-    delivery = scrapy.Field() #商品货期 (需要进入页面)
--- findel_crawl/findel_crawl/spiders/findel.py
+++ findel_crawl/findel_crawl/spiders/findel.py
@@ -6,37 +6,39 @@ from findel_crawl.items import findelItem
 class FindelSpider(scrapy.Spider):
     name = 'findel'
     allowed_domains = ['www.findel-international.com']
-    start_urls = ['https://www.findel-international.com/products/']
+    #start_urls = ['https://www.findel-international.com/products?p=1&show=100/']
 
+    def start_requests(self):
+        first_url = 'https://www.findel-international.com'
+        yield scrapy.Request(first_url,callback=self.parse_col)
+
+    def parse_col(self,response):
+        some_urls = response.css('ul.site-menu.site-menu--top.site-menu--level-1 a.site-menu__link.site-menu__link--level-1::attr(href) ').extract()
+        print(some_urls)        
+        for url in some_urls:
+            real_url = 'https://www.findel-international.com' + url
+            yield scrapy.Request(real_url,callback=self.parse_agerange)  #本来想用meta传递url参数，但是meta参数不可以变化，只好后面使用response.request.url
+            
     def parse(self, response):
         findels = response.css('.product-pod')
         for findel in findels:
             item = findelItem()
             item['title'] = findel.css('.product-pod__title::text').extract_first()
-            item['price'] = findel.css('.product-pod__price::text').extract_first()
-            item['img'] = findel.css('.product-pod__image::attr("src")').extract_first()
             item['link'] = 'https://www.findel-international.com'+findel.css('a.product-pod__link::attr(href)').extract_first()
             item['code'] = item['link'][item['link'].rindex('/')+1:].upper()
-            item['classsify'] = item['link'][37:-len(item['code'])]
-            yield scrapy.Request(url=item['link'], meta={'item': item}, callback=self.parse_detail) #此处进入详情页调用parse_detail
+            item['classsify'] = item['link'][45:-(len(item['code'])+len(item['title'])+2)]
+            yield item
                     
         if response.css('a.pager__link.page-link.page-link--next::attr(data-page)').extract_first() is not None: # 如果有下一页标签,没有表示结束了
             page = response.css('a.pager__link.page-link.page-link--next::attr(data-page)').extract_first()     
-            next = "?p=" + page
-            print(page)
-            url = "https://www.findel-international.com/products" + next
+            next = response.request.url + "?p=" + page + "&show=100"
+            url =next
             yield scrapy.Request(url=url,callback=self.parse,dont_filter=False)
 
-    def parse_detail(self, response):
-        #调用进入详情页获得详细信息
-        item = response.meta['item']
-        if response.css('.product-details__delivery-notice::text').extract_first() is not None:
-            item['delivery'] = response.css('.product-details__delivery-notice::text').extract_first()
-        if response.css('#facetGroup-433').extract_first() is not None:
-            item['nature'] = response.css('#facetGroup-433 option::text').extract()
-        item['describe'] = response.css('.product-description__content div p::text').extract()  
-        yield item 
-
-           
- 
-    
+    def parse_agerange(self, response);
+        Age_ranges = response.css('div.filter-panel__list js-filter-panel-list a.filter-variant::attr(href)').extract()
+        print(Age_ranges)
+        for age_range in Age_ranges:
+            age_url = 'https://www.findel-international.com' + url
+            yield scrapy.Request(age_url,callback=self.parse)
+        
